7cf5dcd3e85763d7185a609283b648d78c52340b
Joonyoung Jeong <mango0727@gmail.com>
2026-04-25 18:32:29 +0900

    Remove all files

--- master/xpdf-4.06/build/goo/CMakeFiles/goo_objs.dir/Trace.cc.o.d
+++
@@ -1,3 +0,0 @@
-goo/CMakeFiles/goo_objs.dir/Trace.cc.o: \
-  /home/joon/assignments/obj14/xpdf-4.06/goo/Trace.cc \
-  /home/joon/assignments/obj14/xpdf-4.06/build/aconf.h
--- master/xpdf-4.06/build/xpdf/CMakeFiles/xpdf_objs.dir/PDFDocEncoding.cc.o.d
+++
@@ -1,6 +0,0 @@
-xpdf/CMakeFiles/xpdf_objs.dir/PDFDocEncoding.cc.o: \
-  /home/joon/assignments/obj14/xpdf-4.06/xpdf/PDFDocEncoding.cc \
-  /home/joon/assignments/obj14/xpdf-4.06/build/aconf.h \
-  /home/joon/assignments/obj14/xpdf-4.06/goo/gmempp.h \
-  /home/joon/assignments/obj14/xpdf-4.06/xpdf/PDFDocEncoding.h \
-  /home/joon/assignments/obj14/xpdf-4.06/xpdf/CharTypes.h
